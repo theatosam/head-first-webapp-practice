0dda0042c85188cf053616eddccadfcfc0b018d8
Ato Sam <atosam23@gmail.com>
2020-06-07 11:51:02 +0000

    Revert "Update do_search function #2"
    This reverts commit 1ba79e8db98454ebb3c7603908802ca7dab82167.

--- vsearch4web.py
+++ vsearch4web.py
@@ -13,13 +13,7 @@ def hello() -> str:
 def do_search() -> str:
     phrase = request.form['phrase']
     letters = request.form['letters']
-    title = 'Here are your results:'
-    results = str(search4letters(phrase, letters))
-    return render_template('results.html',
-                           the_phrase=phrase
-                           the_letters=letters
-                           the_title=title
-                           the_results=results)
+    return str(search4letters(phrase, letters))
 
 
 @app.route('/entry')
