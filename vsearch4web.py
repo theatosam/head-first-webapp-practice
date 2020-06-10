72201ac3ba1ce834d654e8dd85f1c5d24d73db8a
Ato Sam <atosam23@gmail.com>
2020-06-10 19:38:05 +0000

    Add redirect

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from vsearch import search4letters
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello() -> str:
-    return 'Hello World from Flask!'
+def hello() -> '302':
+    return redirect('/entry')
 
 
 @app.route('/search4', methods=['POST'])
